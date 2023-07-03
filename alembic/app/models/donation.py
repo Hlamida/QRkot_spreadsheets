404c07e980df46d727690b825bfe8d372a8a3524
Hlamida <108410042+Hlamida@users.noreply.github.com>
2023-07-03 23:10:20 +0300

    After pytest 2

--- alembic/app/models/donation.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, ForeignKey, Integer, Text
-
-from app.models.base import CharityBase
-
-
-class Donation(CharityBase):
-    user_id = Column(Integer, ForeignKey('user.id'))
-    comment = Column(Text)
